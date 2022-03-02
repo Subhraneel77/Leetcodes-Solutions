e1a217156d5282653e4926deca60166dce488308
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2022-03-02 21:20:21 +0530

    Time: 3 ms (47.29%), Space: 6.4 MB (72.95%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+    int j = 0; // For index of str1 (or subsequence
+ 
+    // Traverse str2 and str1, and
+    // compare current character
+    // of str2 with first unmatched char
+    // of str1, if matched
+    // then move ahead in str1
+    for (int i = 0; i < t.length() && j < s.length(); i++)
+        if (s[j] == t[i])
+            j++;
+ 
+    // If all characters of str1 were found in str2
+    return (j == s.length());
+    }
+};
