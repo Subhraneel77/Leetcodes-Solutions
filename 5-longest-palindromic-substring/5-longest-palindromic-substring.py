0c940284c9a1df31c86fd8db14d0a5a7d73df5be
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2022-06-16 19:28:09 +0530

    Time: 135 ms (98.52%), Space: 14 MB (29.29%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -2,17 +2,17 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if s == s[::-1]: return s
         
-        window_len = 1
+        a = 1
         start = 0
         for i in range(1, len(s)):
             # aaa, from index 1 to index 2, a -> aa or when aa -> aaa, increase max window by 1
-            if i - window_len >= 0 and s[i - window_len:i+1] == s[i - window_len:i+1][::-1]:
-                start = i - window_len
-                window_len += 1
+            if i - a >= 0 and s[i - a:i+1] == s[i - a:i+1][::-1]:
+                start = i - a
+                a += 1
             
             # aba when coming to index 2, we find bab, from a -> bab, increase max window by 2
-            elif i - window_len >= 1 and s[i - window_len - 1:i+1] == s[i - window_len - 1:i+1][::-1]:
-                start = i - window_len - 1
-                window_len += 2
+            elif i - a >= 1 and s[i - a - 1:i+1] == s[i - a - 1:i+1][::-1]:
+                start = i - a - 1
+                a += 2
             
-        return s[start: start + window_len]
+        return s[start: start + a]
