919c97f8f881d28b193b276834df0e33b45eb245
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2022-12-31 10:52:56 +0530

    Time: 6 ms (99.03%), Space: 10.9 MB (43.44%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> indices;
-        for (int i = 0; i < nums.size(); i++) {
-            if (indices.find(target - nums[i]) != indices.end()) {
-                return {indices[target - nums[i]], i};
-            }
-            indices[nums[i]] = i;
+        unordered_map<int,int> indices;
+        for (int i=0;i <nums.size();i++)
+        {
+            if(indices.find(target-nums[i]) != indices.end()){
+                return{indices[target-nums[i]],i};
         }
-        return {};
+        indices[nums[i]]= i;
     }
+    return{};
+}
 };
