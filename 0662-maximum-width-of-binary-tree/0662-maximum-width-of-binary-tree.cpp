5f1de1bb3b636f3ce69c60bdfff957239c5c8de7
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2023-04-20 12:22:39 +0530

    Time: 4 ms (95.06%), Space: 17.5 MB (25.97%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -7,13 +7,13 @@ public:
         int res = 1;
         queue<pair<TreeNode*, int>> q;
         
-        // I am using intialising list
-        q.push({root, 0});      // also can use make_pair
+        
+        q.push({root, 0});      
         
         while(!q.empty())
         {
             int cnt = q.size();
-            // start is the index of root node for first level
+           
             int start = q.front().second;
             int end = q.back().second;
             
@@ -22,18 +22,15 @@ public:
             for(int i = 0; i <cnt; ++i)
             {
                 pair<TreeNode*, int> p = q.front();
-                // we will use it while inserting it children
-                // left child will be 2 * idx + 1;
-                // right chils will be 2 * idx + 2;
+              
                 int idx = p.second - start;
                 
                 q.pop();
                 
-                // if  left child exist
                 if(p.first->left != NULL)
                     q.push({p.first->left, (long long)2 * idx + 1});
                 
-                // if right child exist
+            
                 if(p.first->right != NULL)
                     q.push({p.first->right, (long long) 2 * idx + 2});
             }
