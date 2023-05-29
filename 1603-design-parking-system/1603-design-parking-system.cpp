106a2f561000d2380538d53115362c9c12ec6cb5
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2023-05-29 11:07:03 +0530

    Time: 65 ms (49.23%), Space: 33 MB (88.19%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -0,0 +1,20 @@
+class ParkingSystem {
+private:
+    int spots[3];
+
+public:
+    ParkingSystem(int big, int medium, int small) {
+        spots[0] = big;
+        spots[1] = medium;
+        spots[2] = small;
+    }
+
+    bool addCar(int carType) {
+        if (spots[carType - 1] > 0) {
+            spots[carType - 1]--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
