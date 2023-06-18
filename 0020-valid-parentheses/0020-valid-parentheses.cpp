7a7201dd1e57960fe4fe6711a10c443876346fef
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2023-06-18 18:42:42 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (97.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     bool isValid(string s) {
-        //taking stack for keep tracking the order of the brackets..
-        stack <char> st;
         
+        stack<char> st;
         int len = s.length();
-        for (int i = 0; i<len ; i++)//iterate over each and every elements
-        {
-             //if current element of the string will be opening bracket
-             //then we will just simply push it into the stack
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+        int i;
+        
+        for(i=0;i<len;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
                 st.push(s[i]);
-            else if(s[i] == ')' || s[i] == '}' || s[i] == ']')
-            {
+            else if(s[i]==')' || s[i]=='}' || s[i]==']'){
                 if(st.empty())
                     return false;
-                else if(s[i] == ')' && st.top() != '(')
-                    return false;
-                else if(s[i] == '}' && st.top() != '{')
-                    return false;
-                else if(s[i] == ']' && st.top() != '[')
-                    return false;
-                //if control reaches to that line,
-                //it means we have got the right pair of brackets,
-                //so just pop it.
-                else 
+                else if(s[i]==')' && st.top() !='(')
+                return false;
+                else if(s[i]=='}' && st.top()!='{')
+                return false;
+                else if(s[i]==']' && st.top()!='[')
+                return false;
+                else
                     st.pop();
             }
+                
         }
         if(st.empty())
             return true;
@@ -34,3 +29,4 @@ public:
             return false;
     }
 };
+        
