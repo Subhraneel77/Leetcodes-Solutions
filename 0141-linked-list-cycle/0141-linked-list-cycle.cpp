4ca243c529dac5809fc954c34059fcc99f3ca69e
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2023-09-04 09:37:38 +0530

    Time: 11 ms (49.91%), Space: 8.1 MB (69.03%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        int max=10001;
+        for(int i=0;i<max;i++)
+        {
+            if(head) head=head->next;
+            else return false;
+        }
+        return true;    
+    }
+};
+
