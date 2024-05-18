1d599762856c5bf1e44c79a3e93f863758fe85f2
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2024-05-18 12:19:42 +0530

    Time: 3 ms (80.19%), Space: 15.5 MB (89.89%) - LeetHub

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+        static int distributeCoins(TreeNode* root, TreeNode* parent= NULL) {
+        if (!root) return 0;
+        int  moves=distributeCoins(root->left, root)+distributeCoins(root->right, root);
+        int x=root->val-1;// give x coins to parent node
+        if (parent) parent->val += x; // now parent's coins
+        moves+=abs(x);
+        return moves;
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
