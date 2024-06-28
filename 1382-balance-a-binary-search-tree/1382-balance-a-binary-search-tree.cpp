8a5176c93184b3c34772102700162414ebe807a6
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2024-06-29 00:57:53 +0530

    Time: 72 ms (82.07%), Space: 46.4 MB (87.67%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
-    vector<int> arr;
-    inline void inOrder(TreeNode* root){
-        vector<TreeNode*> stack;
-        TreeNode* node=root;
-        while (node || !stack.empty()){
-            while(node){
-                stack.push_back(node);
-                node=node->left;
-            }
-            node=stack.back();
-            stack.pop_back();
-            
-            arr.push_back(node->val);
-
-            node=node->right;
-        }
-    }
-
-    TreeNode* balanceBST(int l, int r){
-        if (l>r) return NULL;
-        const int m=(l+r)/2;
-        TreeNode* left=(l>m-1)?NULL:balanceBST(l, m-1);
-        TreeNode* right=(m+1>r)?NULL:balanceBST(m+1, r);
-        return new TreeNode(arr[m], left, right);
-    }
-
+    vector<TreeNode*> sortedArr;
     TreeNode* balanceBST(TreeNode* root) {
-        inOrder(root);
-        return balanceBST(0, arr.size()-1);
+        inorderTraverse(root);
+        return sortedArrayToBST(0, sortedArr.size() - 1);
+    }
+    void inorderTraverse(TreeNode* root) {
+        if (root == NULL) return;
+        inorderTraverse(root->left);
+        sortedArr.push_back(root);
+        inorderTraverse(root->right);
+    }
+    TreeNode* sortedArrayToBST(int start, int end) {
+        if (start > end) return NULL;
+        int mid = (start + end) / 2;
+        TreeNode* root = sortedArr[mid];
+        root->left = sortedArrayToBST(start, mid - 1);
+        root->right = sortedArrayToBST(mid + 1, end);
+        return root;
     }
 };
-
-
-
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
