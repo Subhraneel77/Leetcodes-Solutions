f03d278b79847fbc2e03f740db08e33e0f45144f
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2024-08-18 11:46:36 +0530

    Time: 6 ms (55.94%), Space: 9.7 MB (56.48%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        if(n <= 0) return false; // get rid of corner cases 
-        if(n == 1) return true; // base case
-        int t2 = 0, t3 = 0, t5 = 0; //pointers for 2, 3, 5
-        vector<int> k(n);
-        k[0] = 1;
-        for(int i  = 1; i < n ; i ++)
-        {
-            k[i] = min(k[t2]*2,min(k[t3]*3,k[t5]*5));
-            if(k[i] == k[t2]*2) t2++; 
-            if(k[i] == k[t3]*3) t3++;
-            if(k[i] == k[t5]*5) t5++;
+
+        vector<int> dp(n);
+        dp[0] = 1;
+
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+
+        for(int i=1;i<n;i++){
+
+            int twoMul = dp[p1] * 2;
+            int threeMul = dp[p2] * 3;
+            int fiveMul = dp[p3] * 5;
+
+            dp[i] = min(twoMul, min(threeMul,fiveMul ));
+
+            if(dp[i] == twoMul) p1++;
+            if(dp[i] == threeMul) p2++;
+            if(dp[i] ==  fiveMul) p3++;
+            
         }
-        return k[n-1];
+
+        return dp[n-1];
+        
     }
 };
