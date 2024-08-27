704710ed4f9a5d082d050551636c97b9d4edd915
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2024-08-27 10:49:31 +0530

    Time: 0 ms (100.00%), Space: 9 MB (53.24%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+bool helper(int i,int j,string &s,string &p,vector<vector<int>>&dp){
+    // Jai Shree Ram
+    int n=s.size(),m=p.size();
+    if(j==m){
+        return (i==n);
+    }
+    if(dp[i][j]!=-1)return dp[i][j];
+    bool nextStar=false;
+    if(j+1<m&&p[j+1]=='*')
+    nextStar=true;
+    if(nextStar){
+        bool a=false,b=false;
+        // take
+        if(i<n&&(s[i]==p[j]||p[j]=='.'))
+        a=helper(i+1,j,s,p,dp);
+        // not take
+        b=helper(i,j+2,s,p,dp);
+        return dp[i][j]= (a||b);
+    }
+    else{
+        if(i<n&&(s[i]==p[j]||p[j]=='.'))
+        return dp[i][j]= helper(i+1,j+1,s,p,dp);
+    }
+    return dp[i][j]= false;
+}
     bool isMatch(string s, string p) {
-        int n = s.length(), m = p.length();
-        bool dp[n+1][m+1];
-        memset(dp, false, sizeof(dp));
-        dp[0][0] = true;
-        
-        for(int i=0; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(p[j-1] == '*'){
-                    dp[i][j] = dp[i][j-2] || (i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.') && dp[i-1][j]);
-                }
-                else{
-                    dp[i][j] = i > 0 && dp[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.');
-                }
-            }
-        }
-        
-        return dp[n][m];
+        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
+        return helper(0,0,s,p,dp);
     }
 };
