42de9acad1f687d5bc51cc9c2cda00de8ff42630
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2024-09-05 11:16:25 +0530

    Time: 93 ms (74.26%), Space: 116.4 MB (55.88%) - LeetHub

---
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        const int m=rolls.size();
+        const int sum_rolles=accumulate(rolls.begin(), rolls.end(), 0);
+        const int total=(n+m)*mean, miss=total-sum_rolles;
+    //    cout<<total<<", miss="<<miss<<endl;
+        if (miss>6*n || miss<n) return {};
+        auto [q, r]=div(miss, n);
+        vector<int> ans(n, q);
+        fill(ans.begin(), ans.begin()+r, q+1);
+        return ans;
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
