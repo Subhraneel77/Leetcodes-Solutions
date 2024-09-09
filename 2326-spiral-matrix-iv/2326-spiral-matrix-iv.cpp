27cc85e9f14747c722a8831d9c53c6eaae2986fd
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2024-09-09 09:30:46 +0530

    Time: 157 ms (78.81%), Space: 130.5 MB (82.17%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> matrix(m, vector<int>(n, -1));
+        
+        int topRow = 0, bottomRow = m - 1, leftColumn = 0, rightColumn = n - 1;
+        
+        while (head) {
+            // Fill top row
+            for (int col = leftColumn; col <= rightColumn && head; ++col) {
+                matrix[topRow][col] = head->val;
+                head = head->next;
+            }
+            topRow++;
+            
+            // Fill right column
+            for (int row = topRow; row <= bottomRow && head; ++row) {
+                matrix[row][rightColumn] = head->val;
+                head = head->next;
+            }
+            rightColumn--;
+            
+            // Fill bottom row
+            for (int col = rightColumn; col >= leftColumn && head; --col) {
+                matrix[bottomRow][col] = head->val;
+                head = head->next;
+            }
+            bottomRow--;
+            
+            // Fill left column
+            for (int row = bottomRow; row >= topRow && head; --row) {
+                matrix[row][leftColumn] = head->val;
+                head = head->next;
+            }
+            leftColumn++;
+        }
+        
+        return matrix;
+    }
+};
