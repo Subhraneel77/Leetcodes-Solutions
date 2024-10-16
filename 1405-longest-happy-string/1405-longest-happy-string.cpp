b450696c893322a5024b0d49b8bb63b3efe2e529
Subhraneel Haldar <75981967+Subhraneel77@users.noreply.github.com>
2024-10-16 11:20:23 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (90.57%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    std::string longestDiverseString(int a, int b, int c) {
+        int currA = 0, currB = 0, currC = 0;
+        int maxLen = a + b + c, i = 0;
+        std::string result; // Using std::string instead of StringBuilder
+        
+        while (i < maxLen) {
+            if ((currA != 2 && a >= b && a >= c) || (a > 0 && (currB == 2 || currC == 2))) {
+                result += 'a';
+                currA++;
+                currB = 0;
+                currC = 0;
+                a--;
+            } 
+            else if ((currB != 2 && b >= a && b >= c) || (b > 0 && (currA == 2 || currC == 2))) {
+                result += 'b';
+                currB++;
+                currA = 0;
+                currC = 0;
+                b--;
+            } 
+            else if ((currC != 2 && c >= a && c >= b) || (c > 0 && (currA == 2 || currB == 2))) {
+                result += 'c';
+                currC++;
+                currA = 0;
+                currB = 0;
+                c--;
+            }
+            i++;
+        }
+        
+        return result;
+    }
+};
